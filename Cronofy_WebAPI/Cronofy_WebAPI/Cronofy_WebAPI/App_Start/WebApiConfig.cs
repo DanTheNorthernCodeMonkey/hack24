2a7798e2d6e22257cd54997cf42c9b7013acbac1
Carl Jackson <carlanthonyjackson@googlemail.com>
2016-03-19 19:10:47 +0000

    Adding cors Adding AddEvent controller

--- Cronofy_WebAPI/Cronofy_WebAPI/Cronofy_WebAPI/App_Start/WebApiConfig.cs
+++ Cronofy_WebAPI/Cronofy_WebAPI/Cronofy_WebAPI/App_Start/WebApiConfig.cs
@@ -11,6 +11,7 @@ namespace Cronofy_WebAPI
         {
             // Web API configuration and services
 
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- Cronofy_WebAPI/Cronofy_WebAPI/Cronofy_WebAPI/Controllers/AddEventController.cs
+++ Cronofy_WebAPI/Cronofy_WebAPI/Cronofy_WebAPI/Controllers/AddEventController.cs
@@ -1,20 +1,61 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Net.Http;
-//using System.Web.Http;
-//using Cronofy_WebAPI.Helpers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Cronofy;
+using Cronofy_WebAPI.Contracts;
+using Cronofy_WebAPI.Helpers;
 
-//namespace Cronofy_WebAPI.Controllers
-//{
-//    public class AddEventController : ApiController
-//    {
-//        [Route(@"daterange/{startDateString}/{endDateString}")]
-//        public bool Post([FromUri]string startDateString, [FromUri]string endDateString)
-//        {
-//            var parsedStartDateString = DateTimeParser.Parse(startDateString);
-//            var parsedEndDateString = DateTimeParser.Parse(endDateString);
-//        }
-//    }
-//}
+namespace Cronofy_WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/AddEvent")]
+    public class AddEventController : ApiController
+    {
+        private readonly IConfiguration _configuration;
+
+        public AddEventController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route(@"{startDateString}/{endDateString}/{description}/{summary}")]
+        public HttpResponseMessage Post([FromUri]string startDateString, [FromUri]string endDateString, string description, string summary)
+        {
+            try
+            {
+                var cronofy = new CronofyAccountClient(_configuration.AuthToken);
+
+                var calendars = cronofy.GetCalendars();
+
+                var calendarId =
+                    calendars.Where(x => x.Profile.ProviderName == "google").Select(x => x.CalendarId).First();
+
+                var parsedStartDateString = DateTimeParser.Parse(startDateString);
+                var parsedEndDateString = DateTimeParser.Parse(endDateString);
+
+                var uniqueId = Guid.NewGuid();
+
+                var eventBuilder = new UpsertEventRequestBuilder()
+                    .EventId(uniqueId.ToString())
+                    .Summary(summary)
+                    .Description(description)
+                    .Start(parsedStartDateString.Year, parsedStartDateString.Month, parsedStartDateString.Day,
+                        parsedStartDateString.DateTime.Hour, parsedStartDateString.DateTime.Minute)
+                    .End(parsedEndDateString.Year, parsedEndDateString.Month, parsedEndDateString.Day,
+                        parsedEndDateString.DateTime.Hour, parsedEndDateString.DateTime.Minute);
+
+                cronofy.UpsertEvent(calendarId, eventBuilder);
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
