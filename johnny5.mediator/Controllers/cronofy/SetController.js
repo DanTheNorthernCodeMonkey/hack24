44f1c4bdb4f65733ad1c15c0fc2a1cf6304073a1
Dan <dan.ian.shea@gmail.com>
2016-03-20 05:32:02 +0000

    refactor, realigning to the friend coordination application.

--- johnny5.mediator/Controllers/cronofy/SetController.js
+++ johnny5.mediator/Controllers/cronofy/SetController.js
@@ -1,26 +1,38 @@
-/*var pusherService = require('../../services/pusherService');
-var pusher = pusherService.get();*/
+var pusherService = require('../../services/pusherService');
+var pusher = pusherService.get();
 var request = require('request');
+var smsService = require('../../services/smsService');
 
 module.exports.post = function (req, res) {
 
     var url = 'http://hack24cronofy.azurewebsites.net/api/AddEvent/';
 
-    url += req.body.startDate +"/";
-    url += req.body.endDate +"/";
-    url += req.body.description +"/";
-    url += req.body.summary +"/";
+    url += req.body.startDate + "/";
+    url += req.body.endDate + "/";
+    url += req.body.description + "/";
+    url += req.body.summaryInfo;
 
     request({
         url: url,
         method: "POST"
-    }, function (error, response, body){
-        if (error){
+    }, function (error, response, body) {
+        if (error) {
             console.log(error);
             res.send(error);
         }
         else {
             console.log(body);
-            res.send(body );
-        }});
+
+            pusher.trigger('calendar', 'eventAdded', {
+                startDate: req.body.startDate,
+                endDate: req.body.endDate,
+                description: req.body.description,
+                summaryInfo: req.body.summaryInfo
+            });
+
+            smsService.send("You're friend just created a new event, don't miss out");
+
+            res.send(body);
+        }
+    });
 };
